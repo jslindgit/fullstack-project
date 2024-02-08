43607d78abf43cf391e253f035080e01e582bde6
Joonas Lindstedt <68861164+jslindgit@users.noreply.github.com>
2024-02-09 00:04:59 +0200

    Remove build directory from repository

--- project-backend/build/util/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SECRET = exports.PORT = exports.DATABASE_URL = exports.API_KEY = void 0;
-require("dotenv/config");
-exports.API_KEY = process.env.API_KEY;
-exports.DATABASE_URL = process.env.DATABASE_URL;
-exports.PORT = process.env.PORT || 3001;
-exports.SECRET = process.env.SECRET;
--- project-backend/build/util/userProvider.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isValidPassword = void 0;
-const isValidPassword = (password) => {
-    return password.length >= 10;
-};
-exports.isValidPassword = isValidPassword;
