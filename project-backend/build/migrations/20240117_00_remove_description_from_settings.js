72fe0cabdc0e56e4dfc2921253ac49272d63202b
jslindgit <68861164+jslindgit@users.noreply.github.com>
2024-02-15 17:49:09 +0200

    .gitignore build and dist directories

--- project-backend/build/migrations/20240117_00_remove_description_from_settings.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-module.exports = {
-    up: async ({ context }) => {
-        await context.removeColumn('settings', 'store_description');
-    },
-};
--- project-backend/build/util/rollback.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const db_1 = require("./db");
-void (0, db_1.rollbackMigration)();
