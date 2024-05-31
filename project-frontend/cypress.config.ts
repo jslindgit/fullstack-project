4a193559ee021616ecbcba79d67aded5b1ef034f
Joonas Lindstedt <68861164+jslindgit@users.noreply.github.com>
2024-05-31 17:37:02 +0300

    fix e2e tests

--- project-frontend/cypress.config.ts
+++ project-frontend/cypress.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+    env: {
+        VITE_ENV: 'test', // Ensure this matches your pipeline configuration
+    },
     e2e: {
         setupNodeEvents(_on, _config) {
             // implement node event listeners here
